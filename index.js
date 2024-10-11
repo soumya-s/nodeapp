547dc3ea9bfd9274da5e0396c1d9d6c4a7c8c466
soumya <twinklesou23@gmail.com>
2024-10-11 18:22:48 +0530

    New addition module added

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var http = require('http');
+var calc = require('./calc.js');
+
+var sum  = calc.add(2,3)
+console.log("the result is " +sum)
+
+
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type':'text/html'});
     res.end("welcome");
 }).listen(8000)
 
-function add(a,b){
-    return(a+b);
-
-}
-console.log("the result is " +add(2,3))
